c179d5b07bec7c2990033889436afb0467afff95
Yogesh <yogesh@tranktechies.com>
2024-04-16 15:22:30 +0530

    latest

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import Login from "./Screens/Login";
+import Chatscreen from "./Screens/Chatscreen";
 
 function App() {
   return (
     <div className="App">
       <ToastContainer autoClose={2000} />
-      <Login />
+      {/* <Login /> */}
+      <Chatscreen />
     </div>
   );
 }
